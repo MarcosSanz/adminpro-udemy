40528d8de7ccc4e194ea091d6e6fd61568ebd7f5
MarcosS <sanzgonzalezmarcos@gmail.com>
2019-07-16 18:29:14 +0200

    añadidos input a la barra de progreso

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PAGES_ROUTES } from './pages.routes';
+import { FormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
 
@@ -9,23 +10,29 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 
+// temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+
 
 @NgModule({
   declarations: [
     PagesComponent,
     DashboardComponent,
     ProgressComponent,
-    Graficas1Component
+    Graficas1Component,
+    IncrementadorComponent
   ],
   exports: [
     PagesComponent,
     DashboardComponent,
     ProgressComponent,
-    Graficas1Component
+    Graficas1Component,
+    IncrementadorComponent
   ],
   imports: [
     SharedModule,
-    PAGES_ROUTES
+    PAGES_ROUTES,
+    FormsModule
   ]
 })
 
