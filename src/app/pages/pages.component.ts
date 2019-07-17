62c3b44f4b533895079fbb8975151eb53b52b2a6
MarcosS <sanzgonzalezmarcos@gmail.com>
2019-07-17 15:34:08 +0200

    añadidos Fin de la sección 7

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// LLamamos a un script externo en un plugin fuera de angular.
+declare function init_plugins();
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
@@ -10,6 +13,7 @@ export class PagesComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    init_plugins();
   }
 
 }
